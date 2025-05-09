523d1d54667389d132ea7f543e0fbe3acbc17fab
Jeremy Thomas Vyatcheslav Dix <91851746+jerrydix@users.noreply.github.com>
2025-05-09 13:48:00 +0200

    Revert "Setup of project requirements (todo add 3 microservices, genai, setup db, etc.)"

--- client/src/app/services/hello.service.spec.ts
+++ client/src/app/services/hello.service.spec.ts
@@ -1,14 +1,12 @@
 import { TestBed } from '@angular/core/testing';
-import { provideHttpClient} from '@angular/common/http';
+
 import { HelloService } from './hello.service';
 
 describe('HelloService', () => {
   let service: HelloService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [HelloService, provideHttpClient()],
-    });
+    TestBed.configureTestingModule({});
     service = TestBed.inject(HelloService);
   });
 
