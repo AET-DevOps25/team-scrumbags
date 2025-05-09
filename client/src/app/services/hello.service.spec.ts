a0374b4d9d6bd909e563f561271ca84c86e57b62
Jeremy Thomas Vyatcheslav Dix <91851746+jerrydix@users.noreply.github.com>
2025-05-09 13:57:10 +0200

    Revert "Revert "Setup of project requirements (todo add 3 microservices, gena…"
    This reverts commit 39197b3704d819bba9e1325ee13bf4e81ac054bf.

--- client/src/app/services/hello.service.spec.ts
+++ client/src/app/services/hello.service.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient} from '@angular/common/http';
 import { HelloService } from './hello.service';
 
 describe('HelloService', () => {
   let service: HelloService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HelloService, provideHttpClient()],
+    });
     service = TestBed.inject(HelloService);
   });
 
