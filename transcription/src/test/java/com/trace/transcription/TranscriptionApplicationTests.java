06b5a48e077afa5d1bb4fb76f14aa64823a856b5
Jeremy Thomas Vyatcheslav Dix <91851746+jerrydix@users.noreply.github.com>
2025-05-23 01:55:20 +0200

    Initialize Java transcription microservice with Gradle setup

---
+++ transcription/src/test/java/com/trace/transcription/TranscriptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trace.transcription;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranscriptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
