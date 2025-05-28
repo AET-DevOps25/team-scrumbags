acee06cfde119c598190dd16c44f7f439c1f9cba
Umut Yildirir <umut.yildirir@hotmail.com>
2025-05-28 15:12:09 +0200

    initial commit

---
+++ comms-connector/src/main/java/com/trace/comms_connector/CommsConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.trace.comms_connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommsConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommsConnectorApplication.class, args);
+	}
+
+}
