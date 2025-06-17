548ecdfea0002b1f7f4ab6f0d3e3a29042e8ab74
dennis <dp.jandow@gmail.com>
2025-06-17 13:41:02 +0200

    in theory add method to extract one array out of json

--- sdlc-connector/src/main/java/com/trace/sdlc_connector/JsonUtils.java
+++ sdlc-connector/src/main/java/com/trace/sdlc_connector/JsonUtils.java
@@ -4,11 +4,11 @@ import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 import com.jayway.jsonpath.Option;
+import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JsonUtils {
     /**
@@ -39,58 +39,91 @@ public class JsonUtils {
         Map<String, Object> result = new HashMap<>();
 
         for (String path : jsonPaths) {
-            if (path.startsWith("$.")) {
-                Object value = json.read(path);
-                if (value != null) {
-                    String pathWithoutPrefix = path.substring(2);
-                    String[] segments = pathWithoutPrefix.split("\\.");
-
-                    Map<String, Object> current = result;
-                    for (int i = 0; i < segments.length - 1; i++) {
-                        String segment = segments[i];
-                        current.putIfAbsent(segment, new HashMap<String, Object>());
-                        current = (Map<String, Object>) current.get(segment);
-                    }
-
-                    // Add the value at the final level
-                    current.put(segments[segments.length - 1], value);
-                }
+            if (!path.startsWith("$.")) {
+                continue;
             }
+
+            Object value = json.read(path);
+            if (value == null) {
+                continue;
+            }
+
+            String pathWithoutPrefix = path.substring(2);
+            String[] segments = pathWithoutPrefix.split("\\.");
+
+            Map<String, Object> current = result;
+            for (int i = 0; i < segments.length - 1; i++) {
+                String segment = segments[i];
+                if (segment.isEmpty()) {
+                    continue;
+                }
+
+                current.putIfAbsent(segment, new HashMap<String, Object>());
+                current = (Map<String, Object>) current.get(segment);
+            }
+
+            // Add the value at the final level
+            current.put(segments[segments.length - 1], value);
         }
 
         return result;
     }
 
-    public static Map<String, Object> extractArray(DocumentContext json, String arrayPath) {
-        var tmp = arrayPath.split("\\[\\*]");
-        var untilArray = tmp[0];
-        var afterArray = tmp[1];
+    static Pattern arrayRegexPattern = Pattern.compile("((?:\\w+\\.)*)(\\w+)\\[\\*](.*)");
+
+    public static Map<String, Object> extractWithOneArray(DocumentContext json, String arrayPath) {
+        if (!arrayPath.startsWith("$.")) {
+            throw new IllegalArgumentException("Array path must start with '$.': " + arrayPath);
+        }
+
+        List<String> values = json.read(arrayPath);
+        arrayPath = arrayPath.substring(2); // Remove the '$.' prefix
+
+        Matcher matcher = arrayRegexPattern.matcher(arrayPath);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Invalid array path format: " + arrayPath);
+        }
 
         Map<String, Object> result = new HashMap<>();
+
+        // iterate until array segment
         Map<String, Object> current = result;
-        var untilArraySegments = untilArray.split("\\.");
-        for (String segment : untilArraySegments) {
-            if (!segment.isEmpty()) {
-                current = (Map<String, Object>) current.put(segment, new HashMap<String, Object>());
+        String[] segments = matcher.group(1).split("\\.");
+        for (int i = 0; i < segments.length; i++) {
+            if (segments[i].isEmpty()) {
+                continue;
             }
+
+            current.put(segments[i], new HashMap<String, Object>());
+            current = (Map<String, Object>) current.get(segments[i]);
         }
 
-        List<String> arr = json.read(untilArray, List.class);
-        if (arr == null) {
-            return null;
-        }
-        current.put(
-                untilArraySegments[untilArraySegments.length - 1], new ArrayList<Map<String, Object>>()
-        );
+        // extract array segment
+        current.put(matcher.group(2), new ArrayList<Object>());
+        var list = (ArrayList<Object>) current.get(matcher.group(2));
 
-        List<Map<String, Object>> list = (List<Map<String, Object>>) current.get(
-                untilArraySegments[untilArraySegments.length - 1]
-        );
+        var afterArrayPart = matcher.group(3);
+        if (!StringUtils.hasText(afterArrayPart)) {
+            // case that the last capture group is empty
+            list.addAll(values);
+        } else {
+            afterArrayPart = afterArrayPart.substring(1); // remove the leading '.'
+            segments = afterArrayPart.split("\\.");
+            // build Map for rest of the segments
+            for (Object value : values) {
+                current = new HashMap<>();
+                list.add(current);
+                for (int i = 0; i < segments.length - 1; i++) {
+                    if (segments[i].isEmpty()) {
+                        continue;
+                    }
 
-        for (String entry : arr) {
-            list.add(
-                    extract(entry, afterArray.split("\\."))
-            );
+                    current.put(segments[i], new HashMap<String, Object>());
+                    current = (Map<String, Object>) current.get(segments[i]);
+                }
+
+                current.put(segments[segments.length - 1], value);
+            }
         }
 
         return result;
