af12ef00ff789e6f78ac1b26f671965e0a0a9f33
dennis <dp.jandow@gmail.com>
2025-06-20 17:54:58 +0200

    remove pokemon stuff

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { ListView } from './views/list-view/list.view';
-import { PokemonDetailView } from './views/pokemon-detail.view/pokemon-detail.view';
 import { canActivateAuth, canActivateProject } from './guards/auth.guard';
 import { ProjectOverviewView } from './views/project-overview.view/project-overview.view';
 import { ProjectDetailView } from './views/project-detail.view/project-detail.view';
@@ -10,16 +8,6 @@ export const routes: Routes = [
     path: '',
     redirectTo: '/projects',
     pathMatch: 'full',
-  },
-  {
-    path: 'pokemon',
-    component: ListView,
-    canActivate: [canActivateAuth], // Just check if user is signed in
-  },
-  {
-    path: 'pokemon/:id',
-    component: PokemonDetailView,
-    canActivate: [canActivateAuth], // Just check if user is signed in
   },
   {
     path: 'projects',
