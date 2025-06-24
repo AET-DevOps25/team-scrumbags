fe05fb988eb67bd898fe049068e369c632afa596
Jeremy Thomas Vyatcheslav Dix <91851746+jerrydix@users.noreply.github.com>
2025-06-24 22:55:00 +0200

    fix keycloak client id variable in env template

--- client/src/assets/env.template.js
+++ client/src/assets/env.template.js
@@ -6,7 +6,7 @@
   window["env"]["apiUrl"] = "${API_URL}";
   window["env"]["keycloakUrl"] = "${KEYCLOAK_URL}";
   window["env"]["keycloakRealm"] = "${KEYCLOAK_REALM}";
-  window["env"]["keycloakClient"] = "${KEYCLOAK_CLIENT}";
+  window["env"]["keycloakClient"] = "${KEYCLOAK_CLIENT_ID}";
   window["env"]["redirectUrl"] = "${KEYCLOAK_REDIRECT_URL}";
   window["env"]["debug"] = "${DEBUG}";
 })(this);
