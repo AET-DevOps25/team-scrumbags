4856c04a9f1d54d8c50b72d7575a67d0edf261fc
Jeremy Thomas Vyatcheslav Dix <91851746+jerrydix@users.noreply.github.com>
2025-06-24 23:02:06 +0200

    include http in apiUrl in environment.ts

--- client/src/app/environment.ts
+++ client/src/app/environment.ts
@@ -14,7 +14,7 @@ declare global {
 
 export const environment = {
   production: window.env?.production ?? true,
-  apiUrl: window.env?.apiUrl ?? 'localhost:8080',
+  apiUrl: window.env?.apiUrl ?? 'http://localhost:8080',
   keycloakUrl: window.env?.keycloakUrl ?? 'http://localhost:8081',
   keycloakRealm: window.env?.keycloakRealm ?? 'trace',
   keycloakClient: window.env?.keycloakClient ?? 'trace-api',
