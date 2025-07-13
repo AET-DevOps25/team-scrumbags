fc39e79ef3dba338b99da9a62f5bb4ddcdc80e8f
Umut Yildirir <umut.yildirir@hotmail.com>
2025-07-13 15:02:41 +0200

    add mocks so the test doesnt fail

--- comms-connector/src/test/java/com/trace/comms_connector/CommsConnectorApplicationTests.java
+++ comms-connector/src/test/java/com/trace/comms_connector/CommsConnectorApplicationTests.java
@@ -2,9 +2,26 @@ package com.trace.comms_connector;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import com.trace.comms_connector.connection.ConnectionRepo;
+import com.trace.comms_connector.discord.DiscordRestClient;
+import com.trace.comms_connector.user.UserRepo;
 
 @SpringBootTest
 class CommsConnectorApplicationTests {
+
+	@MockitoBean
+	private UserRepo userRepoMock;
+
+	@MockitoBean
+	private ConnectionRepo connectionRepoMock;
+
+	@MockitoBean
+	private DiscordRestClient discordClientMock;
+
+	@MockitoBean
+	private CommsRestClient commsClientMock;
 
 	@Test
 	void contextLoads() {
